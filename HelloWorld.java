6cba15e1580a650a0d5dca7ae5f7e1fc49550ae1
sanjana3110 <83958089+sanjana3110@users.noreply.github.com>
2025-11-11 12:07:02 +0530

    Rename HelloWorld class to Add and modify main method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
-public class HelloWorld {
+public class Add {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
     }
 }
